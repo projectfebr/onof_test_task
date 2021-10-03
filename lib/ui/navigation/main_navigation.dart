97731e833019275af0ddf67e18bc40b5d7e53959
“Aleksandr <projectfebr@gmail.com>
2021-10-04 00:19:29 +0300

    класс MainNavigation, создает роуты

--- lib/ui/navigation/main_navigation.dart
+++ lib/ui/navigation/main_navigation.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:onof_test_task/ui/widgets/auth/auth_model.dart';
 import 'package:onof_test_task/ui/widgets/auth/auth_widget.dart';
 import 'package:onof_test_task/ui/widgets/main_screen/main_screen.dart';
+import 'package:onof_test_task/ui/widgets/main_screen/main_screen_model.dart';
 import 'package:provider/provider.dart';
 
 class MainNavigationRouteNames {
@@ -14,11 +15,14 @@ class MainNavigation {
       ? MainNavigationRouteNames.mainScreen
       : MainNavigationRouteNames.auth;
 
-  final routes = <String, Widget Function(BuildContext)>{
+  final routes = <String, WidgetBuilder>{
     MainNavigationRouteNames.auth: (context) => ChangeNotifierProvider(
           create: (context) => AuthModel(),
           child: const AuthWidget(),
         ),
-    MainNavigationRouteNames.mainScreen: (context) => const MainScreenWidget()
+    MainNavigationRouteNames.mainScreen: (context) => ChangeNotifierProvider(
+          create: (context) => MainScreenModel(),
+          child: const MainScreenWidget(),
+        ),
   };
 }
