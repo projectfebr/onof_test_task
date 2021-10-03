07b13e26cca06e0ef1788068fc5cd3b478893c47
“Aleksandr <projectfebr@gmail.com>
2021-10-04 00:35:27 +0300

    ApiClient содержит методы для отправки запросов

--- lib/domain/api_client/api_client.dart
+++ lib/domain/api_client/api_client.dart
@@ -1,6 +1,23 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:onof_test_task/domain/entity/documents_list_response.dart';
+import 'package:onof_test_task/domain/entity/user_profile.dart';
+import 'package:onof_test_task/ui/widgets/auth/auth_model.dart';
+
+///
+///statusCode: 404 неправильно указан портал
+///statusCode: 500 неприавльные логин и пароль
+
+String makeHost(String portal) {
+  if (portal == 'personal') {
+    return 'https://$portal.onlyoffice.com';
+  } else {
+    return 'https://$portal.onlyoffice.eu';
+  }
+}
 
 class ApiClient {
   Future<String> auth({
@@ -8,21 +25,110 @@ class ApiClient {
     required String password,
     required String portal,
   }) async {
-    var url =
-        Uri.parse('https://$portal.onlyoffice.eu/api/2.0/authentication.json');
-    var response = await http.post(
-      url,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        'userName': username,
-        'password': password,
-      }),
-    );
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${jsonDecode(response.body)}');
-    return jsonDecode(response.body)['response']['token'];
+    final host = makeHost(portal);
+    var url = Uri.parse('$host/api/2.0/authentication.json');
+
+    try {
+      var response = await http
+          .post(
+            url,
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: jsonEncode({
+              'userName': username,
+              'password': password,
+            }),
+          )
+          .timeout(
+            const Duration(seconds: 20),
+            onTimeout: () => throw ApiClientAuthException(
+                ApiClientAuthExceptionType.network),
+          );
+      if (response.statusCode == 404) {
+        return throw ApiClientAuthException(ApiClientAuthExceptionType.portal);
+      }
+      final responseJson = jsonDecode(response.body);
+      _validateResponse(response, responseJson);
+      return jsonDecode(response.body)['response']['token'];
+    } on SocketException {
+      throw ApiClientAuthException(ApiClientAuthExceptionType.network);
+    } on ApiClientAuthException {
+      rethrow;
+    } catch (e) {
+      throw ApiClientAuthException(ApiClientAuthExceptionType.other);
+    }
+  }
+
+/////////////
+
+  Future<UserProfile> getUserProfile({
+    required String token,
+    required String portal,
+  }) async {
+    final host = makeHost(portal);
+    var url = Uri.parse('$host/api/2.0/people/@self');
+    try {
+      var response = await http.get(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': token,
+        },
+      );
+      final json = jsonDecode(response.body);
+      final userProfile = UserProfile.fromJson(json);
+      return userProfile;
+    } on SocketException {
+      throw ApiClientAuthException(ApiClientAuthExceptionType.network);
+    } on ApiClientAuthException {
+      rethrow;
+    } catch (e) {
+      throw ApiClientAuthException(ApiClientAuthExceptionType.other);
+    }
+  }
+
+////////////
+
+  Future<DocumentsList> getDocumentsList({
+    required String token,
+    required String portal,
+    required String api,
+  }) async {
+    final host = makeHost(portal);
+    var url = Uri.parse('$host/$api');
+    try {
+      var response = await http.get(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': token,
+        },
+      );
+      final json = jsonDecode(response.body);
+      final documentsListResponse = DocumentsList.fromJson(json);
+      return documentsListResponse;
+    } on SocketException {
+      throw ApiClientAuthException(ApiClientAuthExceptionType.network);
+    } on ApiClientAuthException {
+      rethrow;
+    } catch (e) {
+      throw ApiClientAuthException(ApiClientAuthExceptionType.other);
+    }
+  }
+
+  void _validateResponse(Response response, Map<String, dynamic> json) {
+    if (response.statusCode == 500) {
+      final int status = json["statusCode"] as int;
+      final code = status is int ? status : 0;
+      if (code == 500) {
+        throw ApiClientAuthException(ApiClientAuthExceptionType.auth);
+      } else {
+        throw ApiClientAuthException(ApiClientAuthExceptionType.other);
+      }
+    }
   }
 }
