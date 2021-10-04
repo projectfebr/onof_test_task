f634b50585a40571d03319ec9f284656e0a34b03
“Aleksandr <projectfebr@gmail.com>
2021-10-04 11:00:38 +0300

    ApiEndpoint - эндпоинты апи

--- lib/domain/api_client/api_addresses.dart
+++ lib/domain/api_client/api_addresses.dart
@@ -1,4 +1,4 @@
-abstract class ApiAddresses {
+abstract class ApiEndpoint {
   static String myDocuments = 'api/2.0/files/@my';
   static String commonDocuments = 'api/2.0/files/@common';
   static String filesByFolderId = 'api/2.0/files';
